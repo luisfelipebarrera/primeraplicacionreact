586bf386bc0db92603a48cbd3e1c1fd267cab65b
luisfelipebarrera <luisfelipe.barrera@gmail.com>
2019-05-16 21:04:00 -0500

    Mi primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+    </div>
+  );
+}
+
+export default App;
